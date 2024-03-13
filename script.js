e229a5b987c1ab7ee98630421565ebb1dbc25259
Bikal <bikaladhikari5@gmail.com>
2024-03-13 18:58:07 +1100

    action for ac and equal

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ const operators = ["%", "/", "*", "-", "+"];
 let lastOperator = "";
 
 const audio = new Audio("./aa.wav");
+
+const buttonAction = (value) => {
+  if (value === "AC") {
+    strToDisplay = "";
+    display();
+    return;
+  }
+
+  if (value === "=" || value === "Enter") {
+    const lc = strToDisplay[strToDisplay.length - 1];
+    if (operators.includes(lc)) {
+      strToDisplay = strToDisplay.slice(0, -1);
+    }
+    return total();
+  }
+};
